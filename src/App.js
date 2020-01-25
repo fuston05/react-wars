2b51d8d211a894bb983fc23fa01d683dc6d52be1
fuston05 <fuston05@yahoo.com>
2020-01-24 20:58:23 -0500

    animated header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { gsap } from "gsap";
 import Pagination from './components/Pagination/Pagination';
 import Card from './components/Card/Card';
 
 import './styles/global.scss';
 import './styles/app.scss';
 
+// ***************************
+window.onload= function(){
+
+  gsap.to( '.mainHeading', { duration: 1.5, opacity: '1' } )
+  gsap.from( '.mainHeading', { duration: 2, y: '-130%', ease: 'circ.out' } )
+};
+
 
 function App() {
   return (
     <div className="App">
-      <h1>React Wars</h1>
+      <h1 className= 'mainHeading'>React Wars</h1>
       <div className= 'cardCont'>
         <Card />
         <Card />
